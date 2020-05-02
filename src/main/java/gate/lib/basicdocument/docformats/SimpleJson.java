eab0621d264721565588221bf81029f03960fb2d
Johann Petrak <johann.petrak@gmail.com>
2020-05-02 15:42:51 +0200

    Add back old code for benchmarking

---
+++ src/main/java/gate/lib/basicdocument/docformats/SimpleJson.java
@@ -0,0 +1,287 @@
+/*
+ * Copyright (c) 2019 The University of Sheffield.
+ *
+ * This file is part of gatelib-basicdocument 
+ * (see https://github.com/GateNLP/gatelib-basicdocument).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gate.lib.basicdocument.docformats;
+
+import com.fasterxml.jackson.jr.ob.JSON;
+import gate.lib.basicdocument.ChangeLog;
+import gate.lib.basicdocument.BdocDocument;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+
+/**
+ * Serialize and deserialize BdocDocument and BdocChangeLog instances as JSON.
+ * 
+ * These are convenience methods to make it easy to serialise and deserialise
+ * BdocDocument and BdocChangeLog instances as JSON. 
+ * 
+ * @author Johann Petrak johann.petrak@gmail.com
+ */
+public class SimpleJson {
+  
+  // 1) Writing to JSON: this is really simple, we essentially just write 
+  // exactly what we have
+  
+  /**
+   * Serialise a BdocDocument to a file. 
+   * 
+   * @param bdoc Bdoc document
+   * @param path the file path where to write to, will get overwritten
+   */
+  public void dump(BdocDocument bdoc, File path) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      jsonbuilder.write(bdoc, path);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not build and save JSON to "+path, ex);
+    }
+  }
+  
+  /**
+   * Serialise a ChangeLog to a file. 
+   * 
+   * @param clog ChangeLog instance
+   * @param path the file path where to write to, will get overwritten
+   */
+  public void dump(ChangeLog clog, File path) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      jsonbuilder.write(clog, path);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not build and save JSON to "+path, ex);
+    }
+  }
+
+  /**
+   * Serialise a BdocDocument to a Writer.
+   * 
+   * @param bdoc BdocDocument instance
+   * @param writer writer to serialise to
+   */
+  public void dump(BdocDocument bdoc, Writer writer) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      jsonbuilder.write(bdoc, writer);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not build and save JSON to writer", ex);
+    }
+    
+  }
+
+  /**
+   * Serialise a ChangeLog to a Writer.
+   * 
+   * @param clog ChangeLog instance
+   * @param writer writer to serialise to
+   */
+  public void dump(ChangeLog clog, Writer writer) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      jsonbuilder.write(clog, writer);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not build and save JSON to writer", ex);
+    }
+    
+  }
+  
+  /**
+   * Serialise a BdocDocument to an OutputStream.
+   * 
+   * @param bdoc BdocDocument instance
+   * @param ostream output stream to serialise to
+   */
+  public void dump(BdocDocument bdoc, OutputStream ostream) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      jsonbuilder.write(bdoc, ostream);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not build and save JSON to output stream", ex);
+    }    
+  }  
+  
+  /**
+   * Serialise a ChangeLog to an OutputStream.
+   * 
+   * @param clog ChangeLog instance
+   * @param ostream output stream to serialise to
+   */
+  public void dump(ChangeLog clog, OutputStream ostream) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      jsonbuilder.write(clog, ostream);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not build and save JSON to output stream", ex);
+    }    
+  }
+
+  /**
+   * Serialise a BdocDocument as a String
+   * 
+   * @param bdoc Bdoc document
+   * @return the generated JSON string
+   */
+  public String dumps(BdocDocument bdoc) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      return jsonbuilder.asString(bdoc);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not build JSON String from BdocDocument", ex);
+    }    
+  }
+  
+  /**
+   * Serialise a BdocDocument as a String
+   * 
+   * @param clog ChangeLog instance
+   * @return the generated JSON string
+   */
+  public String dumps(ChangeLog clog) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      return jsonbuilder.asString(clog);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not build JSON String from ChangeLog", ex);
+    }    
+  }
+  
+  
+  
+  // 2) Load: it seems this works properly out of the box, no need for custom readers
+  
+  // 2.1) BdocDocument
+  /**
+   * Load JSON from stream
+   * @param instream open stream to load from
+   * @return bdoc document instance
+   */
+  public BdocDocument load_doc(InputStream instream) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      return jsonbuilder.beanFrom(BdocDocument.class, instream);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not read BdocDocument from input stream", ex);
+    }
+  }
+
+  /**
+   * Load JSON from File
+   * @param file File to read from
+   * @return bdoc document instance
+   */
+  public BdocDocument load_doc(File file) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      return jsonbuilder.beanFrom(BdocDocument.class, file);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not read BdocDocument from file "+file, ex);
+    }
+  }
+  
+  /**
+   * Load JSON from reader
+   * @param reader the open reader to load from
+   * @return bdoc document instance
+   */
+  public BdocDocument load_doc(Reader reader) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      return jsonbuilder.beanFrom(BdocDocument.class, reader);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not read BdocDocument from reader", ex);
+    }
+  }
+  
+  /**
+   * Load JSON from String
+   * @param json json string
+   * @return bdoc document instance
+   */
+  public BdocDocument loads_doc(String json) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      return jsonbuilder.beanFrom(BdocDocument.class, json);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not read BdocDocument from String", ex);
+    }
+  }
+
+  // 2.1) BdocDocument
+  
+  /**
+   * Load JSON changelog representation from stream
+   * @param instream open stream
+   * @return changelog instance
+   */
+  public ChangeLog load_log(InputStream instream) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      return jsonbuilder.beanFrom(ChangeLog.class, instream);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not read BdocChangeLog from input stream", ex);
+    }
+  }
+  
+  /**
+   * Load JSON changelog representation from File
+   * @param file file to read from
+   * @return changelog instance
+   */
+  public ChangeLog load_log(File file) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      return jsonbuilder.beanFrom(ChangeLog.class, file);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not read BdocChangeLog from input stream", ex);
+    }
+  }
+  
+  /**
+   * Load JSON changelog representation from reader
+   * @param reader open reader
+   * @return changelog instance
+   */
+  public ChangeLog load_log(Reader reader) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      return jsonbuilder.beanFrom(ChangeLog.class, reader);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not read BdocChangeLog from reader", ex);
+    }
+  }
+  
+  /**
+   * Load JSON changelog representation from string
+   * @param json string
+   * @return changelog instance
+   */
+  public ChangeLog loads_log(String json) {
+    JSON jsonbuilder = JSON.std;
+    try {
+      return jsonbuilder.beanFrom(ChangeLog.class, json);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not read BdocChangeLog from String", ex);
+    }
+  }
+  
+}
---
+++ src/test/java/gate/plugin/format/bdoc/benchmarks/SimpleBenchmark.java
@@ -0,0 +1,264 @@
+/*
+ * Copyright (c) 2019 The University of Sheffield.
+ *
+ * This file is part of gateplugin-Format_Bdoc 
+ * (see https://github.com/GateNLP/gateplugin-Format_Bdoc).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package gate.plugin.format.bdoc.benchmarks;
+
+import gate.CreoleRegister;
+import gate.Document;
+import gate.DocumentExporter;
+import gate.Factory;
+import gate.FeatureMap;
+import gate.Gate;
+import gate.creole.Plugin;
+import gate.creole.ResourceInstantiationException;
+import gate.lib.basicdocument.BdocDocument;
+import gate.util.GateException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.xml.stream.XMLStreamException;
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Run simple benchmarking.
+ *
+ * @author Johann Petrak
+ */
+public class SimpleBenchmark {
+
+  public static class BenchData {
+
+    public int n_load = 0;
+    public int n_save = 0;
+    public long load = 0L;
+    public long save = 0L;
+    public long size_save = 0l;
+
+    public void add(BenchData other) {
+      n_load += other.n_load;
+      n_save += other.n_save;
+      load += other.load;
+      save += other.save;
+      size_save += other.size_save;
+    }
+
+    public void addLoad(BenchData other) {
+      n_load += other.n_load;
+      load += other.load;
+    }
+
+    public void addSave(BenchData other) {
+      n_save += other.n_save;
+      save += other.save;
+      size_save += other.size_save;
+    }
+  }
+
+  public Document load(File from) throws ResourceInstantiationException {
+    FeatureMap fm = Factory.newFeatureMap();
+    // fm.put(Document.DOCUMENT_MIME_TYPE_PARAMETER_NAME, mimtypeString);
+    Document doc = (Document) Factory.createResource("gate.corpora.DocumentImpl", fm);
+    return doc;
+  }
+
+  public void save(Document doc, File to, DocumentExporter exporter) throws XMLStreamException, IOException {
+    if (exporter == null) {
+      gate.corpora.DocumentStaxUtils.writeDocument(doc, to);
+    } else {
+      exporter.export(doc, to);
+    }
+  }
+
+  public BenchData copyDocuments(List<File> inFiles, File outDir, String format, DocumentExporter exporter)
+          throws ResourceInstantiationException, XMLStreamException, IOException {
+    BenchData bd = new BenchData();
+    for (File inFile : inFiles) {
+      File outFile = new File(outDir, inFile.getName() + "." + format);
+      long start = System.nanoTime();
+      Document doc = load(inFile);
+      bd.load += (System.nanoTime() - start);
+      bd.n_load += 1;
+      start = System.nanoTime();
+      save(doc, outFile, exporter);
+      bd.save += (System.nanoTime() - start);
+      bd.n_save += 1;
+      bd.size_save = FileUtils.sizeOf(outFile);
+    }
+    return bd;
+  }
+
+  public List<File> fileList(File inDir) {
+    List<File> dirFiles = new ArrayList<>();
+    for (File f : inDir.listFiles()) {
+      dirFiles.add(f);
+    }
+    return dirFiles;
+  }
+
+  /**
+   * Run the benchmarks.Need 2 parameters: input directory with pre-annotated
+   * GATE documents in xml format; empty working directory
+   * <p>
+   * The program outputs the benchmark results to stdout.
+   * <p>
+   * To run this using maven, use something like:
+   * <pre>
+   * {@code
+   * mvn -Dexec.classpathScope=test test-compile exec:java -Dexec.mainClass="gate.plugin.format.bdoc.benchmarks.SimpleBenchmark" -Dexec.args="bench_in bench_out"
+   * }
+   * </pre>
+   *
+   * @param args program arguments
+   * @throws java.io.IOException
+   * @throws gate.util.GateException
+   */
+  public static void main(String[] args) throws IOException, GateException, ResourceInstantiationException, XMLStreamException {
+    if (args.length != 2) {
+      throw new RuntimeException("Need two parameters: inDir, workDir");
+    }
+    SimpleBenchmark bench = new SimpleBenchmark();
+
+    // Load format plugins
+    Gate.init();
+    Gate.getCreoleRegister().registerPlugin(
+            new Plugin.Maven("uk.ac.gate.plugins", "format-fastinfoset", "8.5"));
+    Gate.getCreoleRegister().registerPlugin(
+            new Plugin.Maven("uk.ac.gate.plugins", "format-bdoc", "1.3-SNAPSHOT"));
+
+    File inDir = new File(args[0]);
+    File outDir = new File(args[1]);
+    File outDir1 = new File(outDir, "bench1");
+    File outDir2 = new File(outDir, "bench2");
+    if (!inDir.exists()) {
+      throw new RuntimeException("Input directory must exist and contain GATE XML files");
+    }
+    if (!outDir.exists()) {
+      throw new RuntimeException("Output directory must exist and be empty");
+    }
+    if (outDir1.exists()) {
+      FileUtils.forceDelete(outDir1);
+    }
+    if (outDir2.exists()) {
+      FileUtils.forceDelete(outDir2);
+    }
+    FileUtils.forceMkdir(outDir1);
+    FileUtils.forceMkdir(outDir2);
+
+    Map<String, DocumentExporter> format2exporter = new HashMap<>();
+    CreoleRegister cr = Gate.getCreoleRegister();
+    format2exporter.put("xml", null);
+    format2exporter.put("finf",
+            (DocumentExporter) cr.get("gate.corpora.FastInfosetExporter").getInstantiations().iterator().next());
+    format2exporter.put("bdocjs",
+            (DocumentExporter) cr.get("gate.plugin.format.bdoc.ExporterBdocJson").getInstantiations().iterator().next());
+    format2exporter.put("bdocjs.gz",
+            (DocumentExporter) cr.get("gate.plugin.format.bdoc.ExporterBdocJsonGzip").getInstantiations().iterator().next());
+    format2exporter.put("bdocmp",
+            (DocumentExporter) cr.get("gate.plugin.format.bdoc.ExporterBdocMsgPack").getInstantiations().iterator().next());
+    format2exporter.put("bdocsjs",
+            (DocumentExporter) cr.get("gate.plugin.format.bdoc.ExporterBdocSimpleJson").getInstantiations().iterator().next());
+    format2exporter.put("bdocsjs.gz",
+            (DocumentExporter) cr.get("gate.plugin.format.bdoc.ExporterBdocSimpleJsonGzip").getInstantiations().iterator().next());
+
+    // Get the list of all xml files in indir
+    List<File> inFiles = bench.fileList(inDir);
+    int n = inFiles.size();
+    if (n > 0) {
+      System.err.println("Number of input XML files: " + n);
+    } else {
+      throw new RuntimeException("Input directory does not contain any XML files");
+    }
+
+    // Initially we have the XML files.
+    // we copy to XML files to all formats including XML to outDir1 
+    // every time we then have the documents in that format in outDir1 we 
+    // copy to all formats to OutDir2 
+    // * we have 5 formats
+    // * every format is written to outDir1 once
+    // * every format is read from outDir1 5 times
+    // * every format is written to outDir2 5 times
+    Map<String, BenchData> format2data = new HashMap<>();
+    for (String fmt : format2exporter.keySet()) {
+      format2data.put(fmt, new BenchData());
+    }
+
+    // outermost loop: run everything 10 times
+    for (int i = 0; i < 10; i++) {
+      // outer loop: read from inDir and write to outDir1
+      for (String fmt_from : format2exporter.keySet()) {
+        System.err.println("Iteration " + i + " - Reading XML and writing format " + fmt_from + " to "+outDir1);
+        FileUtils.forceDelete(outDir1);
+        FileUtils.forceMkdir(outDir1);
+        bench.copyDocuments(inFiles, outDir1, fmt_from, format2exporter.get(fmt_from));
+        for (String fmt_to : format2exporter.keySet()) {
+          FileUtils.forceDelete(outDir2);
+          FileUtils.forceMkdir(outDir2);
+          System.err.println("Copying from "+ outDir1 +  " from format " + fmt_from + " to " + outDir2 + " to format " + fmt_to);
+          List<File> fromFiles = bench.fileList(outDir1);
+          BenchData data = bench.copyDocuments(inFiles, outDir2, fmt_to, format2exporter.get(fmt_to));
+          //System.err.println("Time for loading from "+fmt_from+": "+data.load);
+          //System.err.println("Time for saving to    "+fmt_from+": "+data.save);
+          //System.err.println("Total size saving to  "+fmt_from+": "+data.size_save);
+          format2data.get(fmt_from).addLoad(data);
+          format2data.get(fmt_to).addSave(data);
+        }
+      }
+    }
+    File tsvFile = new File(outDir, "bench.tsv");
+    try (FileOutputStream fos = new FileOutputStream(tsvFile);
+            PrintStream ps = new PrintStream(fos)) {
+      ps.println("Format\tload\tsave\tsize");
+      for (String fmt : format2data.keySet()) {
+        BenchData data = format2data.get(fmt);
+        ps.print(fmt);
+        ps.print("\t");
+        ps.print(data.load);
+        ps.print("\t");
+        ps.print(data.save);
+        ps.print("\t");
+        ps.print(data.size_save);
+        ps.println();
+      }
+    }
+    System.err.println("Created file " + tsvFile);
+    File mdFile = new File(outDir, "bench.md");
+    try (FileOutputStream fos = new FileOutputStream(new File(outDir, "bench.tsv"));
+            PrintStream ps = new PrintStream(fos)) {
+      ps.println("Format\tload\tsave\tsize");
+      for (String fmt : format2data.keySet()) {
+        BenchData data = format2data.get(fmt);
+        ps.print(fmt);
+        ps.print("\t");
+        ps.print(data.load);
+        ps.print("\t");
+        ps.print(data.save);
+        ps.print("\t");
+        ps.print(data.size_save);
+        ps.println();
+      }
+    }
+    System.err.println("Created file " + mdFile);
+
+  }
+}
