5e19fa3b114c7dd7c404a7fabf5189ee3ee2d9fe
Johann Petrak <johann.petrak@gmail.com>
2021-02-06 17:44:29 +0100

    Version 1.10

--- examples/bench-loadsave.groovy
+++ examples/bench-loadsave.groovy
@@ -43,7 +43,7 @@ Gate.init()
 Gate.getCreoleRegister().registerPlugin(
             new Plugin.Maven("uk.ac.gate.plugins", "format-fastinfoset", "8.5"));
 Gate.getCreoleRegister().registerPlugin(
-            new Plugin.Maven("uk.ac.gate.plugins", "format-bdoc", "1.10-SNAPSHOT"));
+            new Plugin.Maven("uk.ac.gate.plugins", "format-bdoc", "1.10"));
 cr = Gate.getCreoleRegister()
 
 fmt2exp = [:]
